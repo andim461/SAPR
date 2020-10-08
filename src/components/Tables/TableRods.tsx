8cd514de0ac62c99ddd36795c91eda6be3893d81
andim461 <andim461@yandex.ru>
2020-10-08 18:07:27 +0300

    pretty change

--- src/components/Tables/TableRods.tsx
+++ src/components/Tables/TableRods.tsx
@@ -21,24 +21,46 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons: any = {
-    Add: forwardRef((props, ref: any) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref: any) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref: any) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref: any) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref: any) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref: any) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref: any) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref: any) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref: any) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref: any) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref: any) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref: any) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref: any) => <ViewColumn {...props} ref={ref} />)
-  };
+	Add: forwardRef((props, ref: any) => <AddBox {...props} ref={ref} />),
+	Check: forwardRef((props, ref: any) => <Check {...props} ref={ref} />),
+	Clear: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
+	Delete: forwardRef((props, ref: any) => (
+		<DeleteOutline {...props} ref={ref} />
+	)),
+	DetailPanel: forwardRef((props, ref: any) => (
+		<ChevronRight {...props} ref={ref} />
+	)),
+	Edit: forwardRef((props, ref: any) => <Edit {...props} ref={ref} />),
+	Export: forwardRef((props, ref: any) => <SaveAlt {...props} ref={ref} />),
+	Filter: forwardRef((props, ref: any) => (
+		<FilterList {...props} ref={ref} />
+	)),
+	FirstPage: forwardRef((props, ref: any) => (
+		<FirstPage {...props} ref={ref} />
+	)),
+	LastPage: forwardRef((props, ref: any) => (
+		<LastPage {...props} ref={ref} />
+	)),
+	NextPage: forwardRef((props, ref: any) => (
+		<ChevronRight {...props} ref={ref} />
+	)),
+	PreviousPage: forwardRef((props, ref: any) => (
+		<ChevronLeft {...props} ref={ref} />
+	)),
+	ResetSearch: forwardRef((props, ref: any) => (
+		<Clear {...props} ref={ref} />
+	)),
+	Search: forwardRef((props, ref: any) => <Search {...props} ref={ref} />),
+	SortArrow: forwardRef((props, ref: any) => (
+		<ArrowDownward {...props} ref={ref} />
+	)),
+	ThirdStateCheck: forwardRef((props, ref: any) => (
+		<Remove {...props} ref={ref} />
+	)),
+	ViewColumn: forwardRef((props, ref: any) => (
+		<ViewColumn {...props} ref={ref} />
+	)),
+};
 
 const TableRods = () => {
 	const [columns, setColumns] = useState<Array<Object>>([
@@ -47,7 +69,7 @@ const TableRods = () => {
 			field: 'i',
 			type: 'numeric',
 			filtering: false,
-			align:'center',
+			align: 'center',
 		},
 		{ title: 'Длина', field: 'L', type: 'numeric', filtering: false },
 		{
@@ -55,21 +77,21 @@ const TableRods = () => {
 			field: 'A',
 			type: 'numeric',
 			filtering: false,
-			align:'center',
+			align: 'center',
 		},
 		{
 			title: 'Модуль упругости',
 			field: 'E',
 			type: 'numeric',
 			filtering: false,
-			align:'center',
+			align: 'center',
 		},
 		{
 			title: 'Допускаемое напряжение',
 			field: 'S',
 			type: 'numeric',
 			filtering: false,
-			align:'center',
+			align: 'center',
 		},
 	]);
 	const [data, setData] = useState<Array<Object>>([]);
@@ -79,7 +101,6 @@ const TableRods = () => {
 				title="Стержни"
 				columns={columns}
 				data={data}
-				
 				options={{
 					search: false,
 					sorting: false,
@@ -87,10 +108,8 @@ const TableRods = () => {
 					paging: false,
 				}}
 				icons={tableIcons}
-				
 				editable={{
-					onRowAdd: (newData:any) =>
-						
+					onRowAdd: (newData: any) =>
 						new Promise((resolve, reject) => {
 							setTimeout(() => {
 								setData([...data, newData]);
