74c295a3ee70363529350a5e3ce6e7f87eab2ddf
andim461 <andim461@yandex.ru>
2020-10-13 14:43:39 +0300

    prettier

--- src/components/Tables/TableRods.tsx
+++ src/components/Tables/TableRods.tsx
@@ -1,21 +1,23 @@
-import React, { useState, forwardRef} from 'react';
+import React, { useState, forwardRef } from 'react';
 import MaterialTable from 'material-table';
 import './Tables.css';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import {
+	AddBox,
+	ArrowDownward,
+	Check,
+	ChevronLeft,
+	ChevronRight,
+	Clear,
+	DeleteOutline,
+	Edit,
+	FilterList,
+	FirstPage,
+	LastPage,
+	Remove,
+	SaveAlt,
+	Search,
+	ViewColumn,
+} from '@material-ui/icons';
 
 const tableIcons: any = {
 	Add: forwardRef((props, ref: any) => <AddBox {...props} ref={ref} />),
